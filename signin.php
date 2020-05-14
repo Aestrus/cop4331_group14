98bbfebb1f2264f1517d56167e505877e5ad7eac
John Cope <johnbentcope@gmail.com>
2020-05-14 17:38:49 -0400

    basic styling template

--- signin.php
+++ signin.php
@@ -11,13 +11,15 @@
     <title>Contact Manager</title>
   </head>
   <body>
-    <form class="signincontainer" action="signin.php" method="POST">
-      <h1>Sign In</h1>
-      <label for="email"><b>Email<b/></label>
-      <input type="text" name="email" placeholder="example@example.com" required>
-      <label for="password">Password</label>
-      <input type="password" name="password" placeholder="password" required>
-      <button type="submit">Sign in</button>
-    </form>
+    <div id="formbox">
+      <form class="container" action="signin.php" method="POST">
+        <h1>Sign In</h1>
+        <label for="email"><b>Email<b/></label>
+        <input type="text" name="email" placeholder="example@example.com" required>
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="password" required>
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
   </body>
 </html>
