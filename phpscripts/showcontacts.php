c21a182fa253c79c27d4f777daf463ec80681a99
GeorgeLu00 <goodmew123@gmail.com>
2020-05-17 15:26:35 -0400

    finished all contacts api

--- phpscripts/showcontacts.php
+++ phpscripts/showcontacts.php
@@ -1,35 +1,41 @@
 <?php
-	// source from professor Leinker
+
 	$inData = getRequestInfo();
+  
+  $userId = $inData["userId"];
+	$searchResults = "";
+	$searchCount = 0;
 
-  $userId = (int)$inData["userId"];
-
-	// connecting to the database
 	$conn = new mysqli("localhost", "test", "testing123", "contact_manager");
-
-	// check connection
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		// create query for sql
-    $sql = "SELECT contact_id, first_name, last_name, email, phone_number FROM list_of_contacts";
-    
-    // $sql = "SELECT contact_id, first_name, last_name, email, phone_number FROM list_of_contacts WHERE user_id = $userId";
-
-		// execute query and see if it goes through
-		if( $result = $conn->query($sql) != TRUE )
+		$sql = "SELECT * from list_of_contacts where user_id = $userId";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
 		{
-			returnWithError( $conn->error );
+			while($row = $result->fetch_assoc())
+			{
+				if( $searchCount > 0 )
+				{
+					$searchResults .= ",\n";
+				}
+				$searchCount++;
+				$searchResults .= 'firstName:' . $row["first_name"] . ' lastName:' . $row["last_name"] . ' email:' . $row["email"] . ' phoneNumber:' . $row["phone_number"] . ' contactId:'. $row["contact_id"] ;
+			}
 		}
-    $conn->close();
-    
+		else
+		{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
 	}
-  
-  returnWithError("");
-	
+
+	returnWithInfo( $searchResults );
+
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
@@ -43,7 +49,13 @@
 	
 	function returnWithError( $err )
 	{
-		$retValue = '{"error":"' . $err . '"}';
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
