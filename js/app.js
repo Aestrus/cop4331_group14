7ff5a1580086fb5846b130a53d2aeb9b47bf6704
John Cope <johnbentcope@gmail.com>
2020-05-18 17:57:41 -0400

    signup works

--- js/app.js
+++ js/app.js
@@ -5,19 +5,28 @@ function doSignup()
 {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
 
-  var jsonPayload = '{"email" : "' + email + '", password" : "' + password + '"}';
-  var url = urlBase + '/signup.' + extension;
+  var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "email" : "' + email + '", "password" : "' + password + '"}';
+  var url ='/phpscripts/signup.' + extension;
 
   var xhr = new XMLHttpRequest();
   xhr.open("POST", url, false);
+  console.log(url);
   xhr.setRequestHeader("Content-type" , "application/json; charset=UTF-8");
-  
+  console.log(jsonPayload);
   try
   {
     xhr.send(jsonPayload);
 
-    var jsonObject = JSON.parse( xhr.responseText );
+    var response = xhr.response;
+
+    console.log( response ) ;
+
+    var jsonObject = JSON.parse( response );
+  
+    console.log( jsonObject ); 
 
     userId = jsonObject.id;
 
@@ -28,15 +37,40 @@ function doSignup()
     }
 
     email = jsonObject.email;
-
-    saveCookie();
-
-    window.location.href = "contacts.html";
+    document.getElementById("signupResult").innerHTML = "Sign up successful. Please sign in. You are being redirected.";
+    setTimeout(function(){ window.location.href = "index.html"; }, 5000);
 
   }
+
   catch(err)
   {
     document.getElementById("signupResult").innerHTML = err.message;
+    console.log(err);
+  }
+}
+function doSignin()
+{
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  try
+  {
+
+
+    saveCookie();
   }
 
+  catch
+  {
+
+  }
+
+}
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
--- phpscripts/signup.php
+++ phpscripts/signup.php
@@ -61,7 +61,7 @@
     
     function returnWithInfo( $searchResults )
 	{
-		$retValue = '{"results":' . $searchResults . ',"error":""}';
+		$retValue = '{"results":"' . $searchResults . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
