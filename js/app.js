78386681f0a88edcf7dfa9f107622eba00aa05f6
Aestrus <aestrusx@gmail.com>
2020-05-23 15:53:56 -0400

    Fixed more merge issues.

--- js/app.js
+++ js/app.js
@@ -561,190 +561,188 @@ function deleteCookie() {
 }
 
 function readCookie() {
+  userId = -1;
 
-  function readCookie() {
-    userId = -1;
+  var data = document.cookie;
+  var splits = data.split(",");
 
-    var data = document.cookie;
-    var splits = data.split(",");
-
-    for (var i = 0; i < splits.length; i++) {
-      var current = splits[i].trim();
-      var tokens = current.split("=");
-      if (tokens[0] == "firstName") {
-        userFirstName = tokens[1];
-      }
-      if (tokens[0] == "lastName") {
-        lastName = tokens[1];
-      }
-      if (tokens[0] == "userId") {
-        userId = tokens[1];
-      }
+  for (var i = 0; i < splits.length; i++) {
+    var current = splits[i].trim();
+    var tokens = current.split("=");
+    if (tokens[0] == "firstName") {
+      userFirstName = tokens[1];
     }
-
-    if (userId < 0) {
-      goHome(0);
-    } else {
+    if (tokens[0] == "lastName") {
+      lastName = tokens[1];
+    }
+    if (tokens[0] == "userId") {
+      userId = tokens[1];
     }
   }
 
-  function goHome() {
-    setTimeout(function () { window.location.href = "index.html"; }, 5000);
+  if (userId < 0) {
+    goHome(0);
+  } else {
   }
+}
 
-  function goHome(time) {
-    setTimeout(function () { window.location.href = "index.html"; }, time);
-  }
+function goHome() {
+  setTimeout(function () { window.location.href = "index.html"; }, 5000);
+}
 
-  function goContacts() {
-    setTimeout(function () { window.location.href = "contacts.html"; }, 0000);
-  }
+function goHome(time) {
+  setTimeout(function () { window.location.href = "index.html"; }, time);
+}
 
-  particlesJS(
-    "particles-js",
+function goContacts() {
+  setTimeout(function () { window.location.href = "contacts.html"; }, 0000);
+}
 
-    {
-      particles: {
-        number: {
-          value: 227,
-          density: {
-            enable: true,
-            value_area: 1025.8919341219544,
-          },
-        },
-        color: {
-          value: "#d6cf47",
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#d6cf47",
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: "img/github.svg",
-            width: 100,
-            height: 100,
-          },
-        },
-        opacity: {
-          value: 0.5,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false,
-          },
-        },
-        size: {
-          value: 3,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false,
-          },
-        },
-        line_linked: {
+particlesJS(
+  "particles-js",
+
+  {
+    particles: {
+      number: {
+        value: 227,
+        density: {
           enable: true,
-          distance: 126.26362266116361,
+          value_area: 1025.8919341219544,
+        },
+      },
+      color: {
+        value: "#d6cf47",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
           color: "#d6cf47",
-          opacity: 0.31565905665290905,
-          width: 1.736124811591,
         },
-        move: {
-          enable: true,
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: true,
+        anim: {
+          enable: false,
           speed: 1,
-          direction: "none",
-          random: true,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200,
-          },
+          opacity_min: 0.1,
+          sync: false,
         },
       },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: true,
-            mode: "grab",
-          },
-          onclick: {
-            enable: false,
-            mode: "push",
-          },
-          resize: true,
-        },
-        modes: {
-          grab: {
-            distance: 131.86813186813188,
-            line_linked: {
-              opacity: 0.5828496490759407,
-            },
-          },
-          bubble: {
-            distance: 400,
-            size: 40,
-            duration: 2,
-            opacity: 8,
-            speed: 3,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
         },
       },
-      retina_detect: true,
-    }
-  );
-
-  function goSignIns() {
-    setTimeout(function () {
-      window.location.href = "signin.html";
-    }, 0000);
+      line_linked: {
+        enable: true,
+        distance: 126.26362266116361,
+        color: "#d6cf47",
+        opacity: 0.31565905665290905,
+        width: 1.736124811591,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab",
+        },
+        onclick: {
+          enable: false,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 131.86813186813188,
+          line_linked: {
+            opacity: 0.5828496490759407,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
   }
+);
 
-  function goSignUp() {
-    setTimeout(function () {
-      window.location.href = "signup.html";
-    }, 0000);
-  }
+function goSignIns() {
+  setTimeout(function () {
+    window.location.href = "signin.html";
+  }, 0000);
+}
 
-  function goForgetPassword() {
-    setTimeout(function () {
-      window.location.href = "forgotPassword.html";
-    }, 0000);
-  }
+function goSignUp() {
+  setTimeout(function () {
+    window.location.href = "signup.html";
+  }, 0000);
+}
 
-  function goContacts() {
-    setTimeout(function () { window.location.href = "contacts.html"; }, 0000);
-  }
+function goForgetPassword() {
+  setTimeout(function () {
+    window.location.href = "forgotPassword.html";
+  }, 0000);
+}
 
-  function goSignIns() {
-    setTimeout(function () { window.location.href = "signin.html"; }, 0000);
-  }
+function goContacts() {
+  setTimeout(function () { window.location.href = "contacts.html"; }, 0000);
+}
 
-  function goSignUp() {
-    setTimeout(function () { window.location.href = "signup.html"; }, 0000);
-  }
+function goSignIns() {
+  setTimeout(function () { window.location.href = "signin.html"; }, 0000);
+}
 
-  function goForgetPassword() {
-    setTimeout(function () { window.location.href = "forgotPassword.html"; }, 0000);
-  }
+function goSignUp() {
+  setTimeout(function () { window.location.href = "signup.html"; }, 0000);
+}
+
+function goForgetPassword() {
+  setTimeout(function () { window.location.href = "forgotPassword.html"; }, 0000);
+}
