6207f96008816b1ada960030f6d0eb1abe7799ef
GeorgeLu00 <goodmew123@gmail.com>
2020-05-23 06:20:34 -0400

    added comments

--- phpscripts/search.php
+++ phpscripts/search.php
@@ -8,15 +8,23 @@
 	$searchResults = "";
 	$searchCount = 0;
 
+	// connect to database
 	$conn = new mysqli("localhost", "test", "testing123", "contact_manager");
+
+	// check for error
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
+		// create query
 		$sql = "SELECT contact_id, first_name, last_name, email, phone_number, user_id FROM list_of_contacts WHERE (first_name LIKE '%$search%' OR last_name LIKE '%$search%' OR email LIKE '%$search%' OR phone_number LIKE '%$search%') AND user_id = $userId ORDER BY $filter";
+
+		// execute query and see if it goes through
 		$result = $conn->query($sql);
+
+		// return user information
 		if ($result->num_rows > 0)
 		{
 			while($row = $result->fetch_assoc())
